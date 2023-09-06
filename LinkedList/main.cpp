382b92177b2baff0e8d5644bfff9dc137b765a93
mostafa <mo528884@gmail.com>
2023-09-06 19:54:16 +0300

    updata home work 1

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <assert.h>
 using namespace std;
 struct Node{
     int data;
@@ -28,6 +29,76 @@ public:
         }
         ++length;
     }
+    void insertFront(int val){
+        ++length;
+        Node* tmp=new Node(val);
+        Node* tmp2=Head;
+        Head=tmp;
+        Head->next=tmp2;
+    }
+    void deleteFront(){
+        --length;
+        Node* tmp=Head;
+        Head=Head->next;
+        delete tmp;
+    }
+    int  get_nth(int n){
+        if (n>length){return -1;}
+        --n;
+        Node* tmp=Head;
+        while (n--){
+            tmp=tmp->next;
+        }
+        return tmp->data;
+    }
+    int get_nthBack(int n){
+        return get_nth(length-n);
+    }
+    Node* search(int val){
+        for (Node* tmp=Head;tmp!= nullptr;tmp=tmp->next) {
+            if (tmp->data==val)
+                return tmp;
+        }
+        return nullptr;
+    }
+    Node* searchAndShiftL(int key){
+        Node* tmp=Head;
+        Node* tmpBack=Head;
+        while (tmp!= nullptr){
+            if (tmp->data==key){
+                if (tmp!=Head){
+                    swap(tmp->data,tmpBack->data);
+                }
+                return  tmpBack;
+            }
+            tmpBack=tmp;
+            tmp=tmp->next;
+        }
+        return nullptr;
+    }
+    void checkVerfied(){
+        if (length==0){
+            assert(Head== nullptr&&Tail== nullptr);
+        }else if (length==1){
+            assert(Head==Tail);
+        }else{
+            assert(Head!=Tail);
+        }
+    }
+    bool is_SameData(const linkedList& source){
+        if (source.length==length){
+            Node* Tmp{Head};
+            Node* Tmp2{source.Head};
+            while (Tmp!= nullptr){
+                if (Tmp->data!=Tmp2->data)
+                    return false;
+                Tmp=Tmp->next,Tmp2=Tmp2->next;
+            }
+
+        }else return false;
+
+    }
+    int Get_length(){return length;}
     ~linkedList(){
         Node* tm=Head->next;
         while (Head!= nullptr){
@@ -37,6 +108,7 @@ public:
             tm=tm->next;
         }
     }
+    linkedList& operator=(const linkedList& )=delete;
 };
 
 
@@ -45,8 +117,13 @@ int main() {
     for (int i = 1; i <= 5; ++i) {
         mo.insert(i);
     }
+    for (int i = 10; i <=50 ; i+=10) {
+        mo.insertFront(i);
+    }
+//    cout<<mo.Get_length();
+    mo.deleteFront();
+    mo.deleteFront();
+    cout<<mo.get_nthBack(5)<<endl;
     mo.print();
-    cout<<flush;
-
     return 0;
 }
