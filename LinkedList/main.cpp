6d49215882463fca4d4406b787ac25958558d1d9
mostafa <mo528884@gmail.com>
2023-09-08 00:18:04 +0300

    updatee Homework2

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -108,6 +108,128 @@ public:
             tm=tm->next;
         }
     }
+    void pop_front(){
+        --length;
+        Node *tmp=Head;
+        Head=Head->next;
+        delete tmp;
+    }
+    void pop_back(){
+      --length;
+      Node* tmp=Head;
+        while (tmp->next!= Tail){
+            tmp=tmp->next;
+        }
+        delete Tail;
+        Tail=tmp;
+        Tail->next= nullptr;
+    }
+    void delete_Node_with_key(int key){
+        Node* tmp=Head;
+        if (Head->data==key){
+            pop_front();
+            return;
+        }else if (Tail->data==key){
+            pop_back();
+            return;
+        }
+        while (tmp->next!= nullptr){
+            if (tmp->next->data==key){
+                Node* tmpCur=tmp->next->next;
+                delete tmp->next;
+                tmp->next=tmpCur;
+                return;
+            }
+            tmp=tmp->next;
+        }
+        assert(false);
+    }
+    void swap_pairs(){
+        Node* tmp=Head;
+        while ((tmp!= nullptr)&&tmp->next!= nullptr){
+            swap(tmp->data,tmp->next->data);
+            tmp=tmp->next->next;
+        }
+    }
+    void reverse(){
+        Node* pervNode= nullptr;
+        Node* curN=Head;
+        Node* NextNode=Head->next;
+        int cur=0;
+        while (curN!=nullptr){
+            curN->next=pervNode;
+            pervNode=curN;
+            curN=NextNode;
+            if (NextNode!= nullptr)
+            NextNode=NextNode->next;
+            ++cur;
+        }
+        swap(Head,Tail);
+    }
+    void delete_even_poss(){
+        bool isEven= false;
+        Node* tmp=Head;
+        Node* tmpBack= nullptr;
+        while (tmp!= nullptr){
+            if (isEven){
+                --length;
+                if (tmp==Tail){
+                    pop_back();
+                    break;
+                }
+                tmpBack->next=tmp->next;
+                delete tmp;
+                tmp=tmpBack;
+            }
+            tmpBack=tmp,tmp=tmp->next;
+            isEven=!isEven;
+        }
+
+    }
+    void insert_sort(int val){
+        if (length==0){
+            insert(val);
+        }else{
+            if (Head->data>val) {
+                insertFront(val);
+                return;
+            }
+            Node* tmp=Head;
+            while (tmp->next!= nullptr){
+                if (tmp->data<=val&&tmp->next->data>=val){
+                    Node* tmpNext=tmp->next;
+                    tmp->next=new Node(val);
+                    tmp->next->next=tmpNext;
+                    return;
+                }
+                tmp=tmp->next;
+            }
+            Tail->next=new Node(val);
+            Tail->next->next= nullptr;
+            Tail=Tail->next;
+        }
+    }
+    void erase_th(int poss){
+        if (poss==0){
+            this->pop_front();
+            return;
+        }else if (poss==length){
+            this->pop_back();
+            return;
+        }
+        Node *tm=Head,*tmback=Head;
+        int cnt{0};
+        while (tm!= nullptr){
+            if (cnt==poss){
+                Node* tmp=tm->next;
+                delete tm;
+                tmback->next=tmp;
+                return;
+            }
+            ++cnt,tmback=tm,tm=tm->next;
+        }
+
+    }
     linkedList& operator=(const linkedList& )=delete;
 };
 
@@ -121,9 +243,24 @@ int main() {
         mo.insertFront(i);
     }
 //    cout<<mo.Get_length();
-    mo.deleteFront();
-    mo.deleteFront();
-    cout<<mo.get_nthBack(5)<<endl;
-    mo.print();
+//    mo.erase_th(mo.Get_length());
+//    mo.delete_Node_with_key(40);
+//    mo.swap_pairs();
+//    mo.reverse();
+
+//    cout<<mo.Get_length()<<endl;
+//    mo.print();
+//    cout<<endl;
+//    mo.delete_even_poss();
+//    cout<<mo.Get_length()<<endl;
+//    mo.print();
+    linkedList sos;
+    sos.insert_sort(5);
+    sos.insert_sort(4);
+    sos.insert_sort(1);
+    sos.insert_sort(0);
+    sos.insert_sort(2);
+    sos.print();
+
     return 0;
 }
