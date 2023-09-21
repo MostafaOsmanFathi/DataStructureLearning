47499da672dd54ff26b2fc98c12918d46f157c93
mostafa <mo528884@gmail.com>
2023-09-22 01:56:08 +0300

    homework 2

--- Doubly Linked List/main.cpp
+++ Doubly Linked List/main.cpp
@@ -149,6 +149,107 @@ public:
 
     ///============Homework 1============
 
+    ///============Homework 2============
+    Node* find_middle(){
+        Node* l=Head,*r=Tail;
+        while (l!=r){
+
+            l=l->next;
+            if (l==r)
+                return l;
+            r=r->prev;
+
+        }
+        return l;
+    }
+    void swapNode(Node* N1,Node* N2){
+        if (N1->next==N2||N2->next==N1){
+            Node* NNxx=N2->next;
+            Node* NPrr=N1->prev;
+
+            NNxx->prev=N1;
+            N1->next=NNxx;
+            N1->prev=N2;
+            N2->next=N1;
+            NPrr->next=N2;
+            N2->prev=NPrr;
+        }else if (N1==Head||N2==Tail){
+            Node* N1Nxt=N1->next;
+            Node* N2Prv=N2->prev;
+
+            N1Nxt->prev=N2;
+            N2->next=N1Nxt;
+            N2->prev= nullptr;
+            N2Prv->next=N1;
+
+            N1->next= nullptr;
+            N1->prev=N2Prv;
+            Head=N2,Tail=N1;
+//            swap(Head,Tail);
+        }else{
+            Node *N1Prv = N1->prev;
+            Node *N1Nxt = N1->next;
+
+            Node *N2Prv = N2->prev;
+            Node *N2Nxt = N2->next;
+
+            N1Prv->next = N2;
+            N2->prev = N1Prv;
+            N2->next = N1Nxt;
+            N1Nxt->prev = N2;
+
+            N2Prv->next = N1;
+            N1->prev = N2Prv;
+            N1->next = N2Nxt;
+            N2Nxt->prev = N1;
+        }
+
+    }
+    void swapFB(int nth){
+        int nthC{nth};
+        Node* tmp1=Head,*tmp2=Tail;
+        while (nthC--){
+            tmp1=tmp1->next;
+            tmp2=tmp2->prev;
+        }
+
+        swapNode(tmp1,tmp2);
+
+    }
+    void mergList(const linkedList& source){
+        Node* tmp1=Head;
+        Node* tmp2=source.Head;
+        while (tmp1||tmp2){
+            if (tmp1&&tmp2){
+                if (tmp1->data<=tmp2->data){
+                    tmp1=tmp1->next;
+                    continue;
+                }else{
+                    Node* tmp2Nx=tmp2->next;
+                    if (tmp1==Head){
+                        linkNode(nullptr,tmp2);
+                        linkNode(tmp2,Head);
+                        Head=tmp2;
+                    }else{
+                        linkNode(tmp1->prev,tmp2);
+                        linkNode(tmp2,tmp1);
+                    }
+                    tmp2=tmp2Nx;
+//                    tmp1=tmp1->next;
+                }
+            }else if (tmp1){
+                tmp1=tmp1->next;
+            }else if (tmp2){
+                linkNode(Tail,tmp2);
+                Tail=tmp2;
+                tmp2->next= nullptr;
+            }
+        }
+
+    }
+
+    ///============Homework 2============
+
 };
 ostream& operator<<(ostream& os,const linkedList& li){
     for (Node* tmp=li.Head;tmp!= nullptr;tmp=tmp->next) {
@@ -177,12 +278,29 @@ int main() {
 //    mos.delete_even_poss();
 //    mos.delete_even_poss(true);
 
-//    mos.insert_end(1);
-//    mos.insert_end(2);
-//    mos.insert_end(5);
-//    mos.insert_end(2);
-//    mos.insert_end(1);
+    mos.insert_end(1);
+    mos.insert_end(2);
+    mos.insert_end(3);
+    mos.insert_end(4);
+    mos.insert_end(5);
+    mos.insert_end(6);
 //cout<<mos.is_Palindrome();
+//    cout<<mos.find_middle()->data;
+//
+//    cout<<mos;
+//    mos.swapFB(0);
+//    mos.swapFB(1);
+//    cout<<mos;
 
+    linkedList l1,l2;
+    l1.insert_end(1);
+    l1.insert_end(2);
+    l1.insert_end(4);
+    l1.insert_end(6);
+
+    l2.insert_end(3);
+    l2.insert_end(5);
+    l1.mergList(l2);
+    cout<<l1;
     return 0;
 }
